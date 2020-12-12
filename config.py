7a107978008e90cf138b290d4cbfceaa1b619610
695509060@qq.com <695509060@qq.com>
2020-12-12 13:34:53 +0800

    up

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ SentenceLength = 256
 BalanceNum = 5000
 
 # 计算BatchSize
-NormalSteps = 2000
+NormalSteps = 4000
 OceTrainCount = len(open(OceTrainPath, 'r', encoding='utf-8').readlines())
 OcnTrainCount = len(open(OcnTrainPath, 'r', encoding='utf-8').readlines())
 TnewsTrainCount = len(open(TnewsTrainPath, 'r', encoding='utf-8').readlines())
